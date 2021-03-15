6145aeabbdd65997ede88a49e57a7bbeb3da205a
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-15 23:45:28 +0900

    2-4. 모듈 시스템과 웹팩 설정

---
+++ WordRelay.jsx
@@ -0,0 +1,18 @@
+const React = require('react');
+const { Component } = React;
+
+class WordRelay extends Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+module.exports = WordRelay;
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,18 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+    name: 'word-realy-setting',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    entry: {
+        app: ['./client']
+    }, // 입력
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    }, // 출력
 };
